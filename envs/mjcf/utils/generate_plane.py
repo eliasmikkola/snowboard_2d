c38c4f5c6cf87d95ac1e36f94ffe17f4e8727200
Elias Mikkola <elias@singa.com>
2023-04-08 09:58:11 +0300

    plane generation changes to fix memory crash

--- envs/mjcf/utils/generate_plane.py
+++ envs/mjcf/utils/generate_plane.py
@@ -8,7 +8,7 @@ import pybullet_data
 from pybullet_envs.scene_abstract import Scene
 import pybullet
 import numpy as np
-
+import matplotlib.pyplot as plt
 class SlopeScene(Scene):
     
 
@@ -16,12 +16,16 @@ class SlopeScene(Scene):
     slope_halflen = 105 * 0.25  # FOOBALL_FIELD_HALFLEN
     slope_halfwidth = 50 * 0.25  # FOOBALL_FIELD_HALFWID
     slopeLoaded = 0  
-    
+
     def episode_restart(self, bullet_client, difficulty=0):
         self.difficulty = difficulty
         self._p = bullet_client
         Scene.episode_restart(self, bullet_client)  # contains cpp_world.clean_everything()
         if (self.slopeLoaded == 0):
+            self.collision_shape_id = -1
+            chess_path = os.path.join(os.path.dirname(__file__), "chess.png")
+            self.textureId = self._p.loadTexture(chess_path)
+            
             self.slopeLoaded = 1
 
             filename = os.path.join(pybullet_data.getDataPath(), "plane_stadium.sdf")
@@ -44,13 +48,13 @@ class SlopeScene(Scene):
         self._p.configureDebugVisualizer(pybullet.COV_ENABLE_GUI, 0)
         self._p.configureDebugVisualizer(pybullet.COV_ENABLE_DEPTH_BUFFER_PREVIEW, 1)
         
-    def generate_sine_plane(self, steepness=0.3, amplitude=0.3, frequency=0.3):
+    def generate_sine_plane(self, steepness=0.3, amplitude=0.3, frequency=0.3, render_mode=None):
         # clear previous planes
-        if self.terrain_plane != None:
-            self._p.removeBody(self.terrain_plane)
-            # remove collision shape
-            self._p.removeCollisionShape(self.terrainShape)
-            self.terrain_plane = None
+        # if self.terrain_plane != None:
+        #     self._p.removeBody(self.terrain_plane)
+        #     # remove collision shape
+        #     self._p.removeCollisionShape(self.collision_shape_id)
+        #     self.terrain_plane = None
         # TODO: add to args
         numHeightfieldRows = 50
         numHeightfieldColumns = 1000
@@ -71,6 +75,7 @@ class SlopeScene(Scene):
         
         heightfieldData
         col,row = heightfieldData.shape
+        heightfieldData_2d = heightfieldData
         heightfieldData = heightfieldData.reshape(-1)
         
         # meshScaling 
@@ -81,11 +86,28 @@ class SlopeScene(Scene):
         terrain_pos = [0,0,0]
         
         # create collision shape
-        self.terrainShape = self._p.createCollisionShape(shapeType=self._p.GEOM_HEIGHTFIELD, 
-            meshScale=meshScale,         
-            heightfieldData=heightfieldData, 
-            numHeightfieldRows=row, 
-            numHeightfieldColumns=col)
+        if self.collision_shape_id >= 0 and render_mode != 'rgb_array':
+            previous_collision_id = self.collision_shape_id
+            if self.terrain_plane != None:
+                self._p.removeBody(self.terrain_plane)
+            self.collision_shape_id = self._p.createCollisionShape(shapeType=self._p.GEOM_HEIGHTFIELD, 
+                meshScale=meshScale,         
+                heightfieldData=heightfieldData, 
+                numHeightfieldRows=row, 
+                numHeightfieldColumns=col,
+                replaceHeightfieldIndex = previous_collision_id
+                )
+        else:
+            self.collision_shape_id = self._p.createCollisionShape(shapeType=self._p.GEOM_HEIGHTFIELD, 
+                meshScale=meshScale,         
+                heightfieldData=heightfieldData, 
+                numHeightfieldRows=row, 
+                numHeightfieldColumns=col,
+                )
+
+        # Print information about each shape
+        # for shape in shapes:
+        #     print("SHAPE", shape)
             
         # create visual shape from terrainShape
         # terrainVisualShape = self._p.createVisualShape(shapeType=self._p.GEOM_MESH, 
@@ -94,13 +116,32 @@ class SlopeScene(Scene):
         #     meshScale=meshScale)
         
 
-
         terrain = self._p.createMultiBody(0, 
-            baseCollisionShapeIndex=self.terrainShape,
+            baseCollisionShapeIndex=self.collision_shape_id,
             # baseVisualShapeIndex=terrainVisualShape, 
             physicsClientId=self._p._client)
-        # # get slope angle from points
-        # slope_angle = np.arctan((heightfieldData[0] - heightfieldData[-1])/(numHeightfieldColumns*meshScale[0]))*180/np.pi
+        # fit a line to the points
+        # take only one row as theyre all the same
+        line = np.polyfit(np.arange(numHeightfieldColumns), heightfieldData_2d[0], 1)
+        # get angle of the line from two points x2,y2
+        y0 = line[0]
+        y1 = line[1]
+        x0 = 0
+        x1 = meshScale[0]
+        slope_angle = np.arctan((y1-y0)/(x1-x0))
+        # # in degrees
+        # slope_angle = slope_angle * 180 / np.pi
+        # # in radians
+
+        # print("\n\n####\n####\nslope angle: ", slope_angle)
+        
+
+
+        # plot the line
+        # plt.plot(np.arange(row), np.polyval(line, np.arange(row)))
+        # # plot the sine wave from heightfieldData_2d[0]
+        # plt.plot(np.arange(row), heightfieldData_2d[0])
+        # plt.show()
         # print("slope angle: ", slope_angle)
         
         # # set terrain friction
@@ -123,10 +164,9 @@ class SlopeScene(Scene):
         self.terrain_plane = terrain
         self._p.changeVisualShape(self.terrain_plane, -1, rgbaColor=[1,1,1, 1])
         # add "chess.png" texture to the plane from absolute path
-        chess_path = os.path.join(os.path.dirname(__file__), "chess.png")
-        textureId = self._p.loadTexture(chess_path)
-        self._p.changeVisualShape(terrain, -1, textureUniqueId=textureId)
-    
+        self._p.changeVisualShape(terrain, -1, textureUniqueId=self.textureId)
+        
+        return slope_angle
 class SinglePlayerSlopeScene(SlopeScene):
   "This scene created by environment, to work in a way as if there was no concept of scene visible to user."
   multiplayer = False
